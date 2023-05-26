158009df5d8c74f6bfdfbf8d10256b59a39054e2
Luis_Resolution <itsluisg@gmail.com>
2023-05-26 16:55:19 +0200

    Smaller improvements on the codebase

--- Program.cs
+++ Program.cs
@@ -1,16 +1,24 @@
 ﻿using System.Diagnostics;
 
 public class Program {
-    private static int[] collection = new int[500000000];
-
     static void Main(string[] args) {
-        Benchmark(BenchmarkFor);
-        Benchmark(BenchmarkForCacheLength);
-        Benchmark(BenchmarkForeach);
-        Benchmark(BenchmarkWhile);
-        Benchmark(BenchmarkWhileCacheLength);
-        Benchmark(BenchmarkDoWhile);
-        Benchmark(BenchmarkDoWhileCacheLength);
+        new Benchmarker(1000000000);
+    }
+}
+
+public class Benchmarker {
+    private static int[] collection = new int[0];
+
+    public Benchmarker(int operations) {
+        collection = new int[operations];
+
+        Benchmark(For);
+        Benchmark(ForWithCache);
+        Benchmark(Foreach);
+        Benchmark(While);
+        Benchmark(WhileWithCache);
+        Benchmark(DoWhile);
+        Benchmark(DoWhileWithCache);
     }
 
     private static void Benchmark(Action action) {
@@ -18,28 +26,28 @@ public class Program {
         sw.Start();
         action.Invoke();
         sw.Stop();
-        Console.WriteLine($"{sw.Elapsed.TotalMilliseconds} Milliseconds has passed on: {action.Method.Name}");
+        Console.WriteLine($"Benchmarking: {sw.Elapsed.Seconds}.{sw.Elapsed.Milliseconds}s -> {action.Method.Name}");
     }
 
-    private static void BenchmarkFor() {
+    private static void For() {
         for(int i = 0; i < collection.Length; i++) {
             _ = i;
         }
     }
 
-    private static void BenchmarkForCacheLength() {
+    private static void ForWithCache() {
         for(int i = 0, n = collection.Length; i < n; i++) {
             _ = i;
         }
     }
 
-    private static void BenchmarkForeach() {
+    private static void Foreach() {
         foreach(int i in collection) {
             _ = collection[i];
         }
     }
 
-    private static void BenchmarkWhile() {
+    private static void While() {
         int i = 0;
         while(i < collection.Length){
             _ = collection[i];
@@ -47,7 +55,7 @@ public class Program {
         }
     }
 
-    private static void BenchmarkWhileCacheLength() {
+    private static void WhileWithCache() {
         int i = 0;
         int length = collection.Length;
         while(i < length){
@@ -56,7 +64,7 @@ public class Program {
         }
     }
 
-    private static void BenchmarkDoWhile() {
+    private static void DoWhile() {
         int i = 0;
         do {
             _ = collection[i];
@@ -65,7 +73,7 @@ public class Program {
         while(i < collection.Length);
     }
 
-    private static void BenchmarkDoWhileCacheLength() {
+    private static void DoWhileWithCache() {
         int i = 0;
         int length = collection.Length;
         do {
